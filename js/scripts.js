9128a67c8b8620d6e2f98370950ea129e6c9b1b8
Carolina Knoll <carolinasknoll@gmail.com>
2017-07-18 14:42:35 -0300

    Refactor names, texts and json data

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,32 @@
-// some more js magic will be here soon!
 $(document).ready(function() {
 
   if (navigator.geolocation) {
-
     navigator.geolocation.getCurrentPosition(function(position) {
 
-      // customize url to call api
       var url = 'https://api.wunderground.com/api/4895338969fb790e/geolookup/conditions/q/autoip.json';
-
       $.getJSON(url, function(json) {
 
-        // feed city, state and country elements with api response data
-        $('#weatherCity').html(json.current_observation.display_location.city);
-        $('#weatherState').html(json.current_observation.display_location.state);
-        $('#weatherCountry').html(json.current_observation.display_location.country);
+        console.log('json.current_observation', json.current_observation);
 
-        // convert temperature from default json data kelvin value to be displayed as celsius
-        var celsiusTemp = json.current_observation.temp_c;
+        // feed page elements with the returned json data
+        $('#country').html(`${json.current_observation.display_location.full}`);
+        $('#state').html(`${json.current_observation.display_location.state},`);
 
-        // display weather temperature
-        $('#weatherTemperature').text(celsiusTemp + ' ºC');
+        $('#temperature').text(`${json.current_observation.temp_c} ºC`);
+        $('#windSpeed').html(`${json.current_observation.wind_kph} m/s`);
+        $('#description').html(`${json.current_observation.weather}`);
+        $('#observationTime').html(`${json.current_observation.observation_time}`);
 
-        // display weather icon
-        // var weatherIcon = json.weather[0].icon;
-        // var weatherIconURL = 'http://openweathermap.org/img/w/' + weatherIcon + '.png';
-        // $('#weatherTemperatureIcon').html('<img src=' + weatherIconURL + '>');
-
-        // display weather description
-        //$('#weatherDescription').html(json.weather[0].description);
-
-        // display weather wind speed
-        $('#weatherWindSpeed').html(json.wind_kph + ' m/s');
-
-        // change to imperial unit values, currently only changes to fahrenheit temperature
+        // change to imperial unit values (fahrenheit / mph)
         $('#convertImperial').on('click', function () {
-          let fahrenTemp = ((9 * celsiusTemp) / 5) + 32;
-          $('#weatherTemperature').text(fahrenTemp.toFixed(1) + ' ºF');
+          $('#temperature').text(`${json.current_observation.temp_f} ºF`);
+          $('#windSpeed').html(`${json.current_observation.wind_mph} m/h`);
         });
 
-        // change to metric unit values, currently to default celsius temperature
+        // change to metric unit values (celsius / kph)
         $('#convertMetric').on('click', function () {
-          $('#weatherTemperature').text(celsiusTemp + ' ºC');
+          $('#temperature').text(`${json.current_observation.temp_c} ºC`);
+          $('#windSpeed').html(`${json.current_observation.wind_kph} k/h`);
         });
 
       });
